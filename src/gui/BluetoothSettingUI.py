00a2fee10eecc32acc34415fdaf446677ba38c62
p-chanmin <pc_m111@naver.com>
2023-03-25 18:59:47 +0900

    #19 디바이스 연결 및 연결 상태 확인

--- src/gui/BluetoothSettingUI.py
+++ src/gui/BluetoothSettingUI.py
@@ -1,12 +1,19 @@
 import tkinter as tk
 import asyncio
 from bleak import BleakScanner
+from bleak import BleakClient
 
 
 class BluetoothSettingUI:
-    def __init__(self, homeclassInstance):
-        self.devices = {}
-        self.__window = self.__createUI(homeclassInstance)
+    __instance = None  # Singleton 인스턴스를 저장할 클래스 변수
+
+    def __new__(cls, homeclassInstance):
+        if cls.__instance is None:
+            cls.__instance = super().__new__(cls)
+            cls.__instance.devices = {}
+            cls.__instance.client = None
+        cls.__instance.__window = cls.__instance.__createUI(homeclassInstance)
+        return cls.__instance
 
     def __createUI(self, homeclassInstance):
         window = tk.Tk()
@@ -17,14 +24,21 @@ class BluetoothSettingUI:
         device_label = tk.Label(window, text="Select Bluetooth Device:")
         device_label.pack()
 
-        def connect_device():
+        async def connect_device():
             selected_device = device_listbox.get(device_listbox.curselection())
-            if(self.devices[selected_device]):
-                print(f"{selected_device} - {self.devices[selected_device]}")
-
-                # 연결 완료 시
-                homeclassInstance.printer_text.set(f"print connect : {selected_device}")
-            # Add code to connect to selected device here
+            if selected_device in self.devices:
+                print(f"Connecting to {selected_device} - {self.devices[selected_device]}")
+                address = self.devices[selected_device]
+                try:
+                    self.client = BleakClient(address)
+                    await self.client.connect()
+                    print(f"Connected to {selected_device}")
+                    # 연결 완료 시
+                    homeclassInstance.printer_text.set(f"print connect : {selected_device}")
+                except Exception as e:
+                    print(f"Failed to connect to {selected_device}: {e}")
+            else:
+                print(f"No device selected")
 
         def on_select(event):
             selected_device = device_listbox.get(device_listbox.curselection())
@@ -60,12 +74,24 @@ class BluetoothSettingUI:
         search_button.pack(side=tk.LEFT, padx=10)
 
         # create a button to connect to the selected Bluetooth device
-        connect_button = tk.Button(button_frame, text="Connect", command=connect_device)
+        connect_button = tk.Button(button_frame, text="Connect", command=lambda: asyncio.run(connect_device()))
         connect_button.pack(side=tk.LEFT, padx=10)
 
+        connect_text = "default"
+        if(self.client is None):
+            print("None")
+            connect_text = "Bluetooth Status: Disconnected"
+        else:
+            print("not None")
+            connect_text = "Bluetooth Status: connected"
         # create a label for the Bluetooth status
-        status_label = tk.Label(window, text="Bluetooth Status: Disconnected")
+        status_label = tk.Label(window, text=connect_text)
         status_label.pack()
+
+        self.printer_text = tk.StringVar()
+        self.printer_text.set("print connect : -")
+        printer_label = tk.Label(window, textvariable=self.printer_text)
+        printer_label.pack()
 
         return window
 
@@ -74,7 +100,3 @@ class BluetoothSettingUI:
 
     def __end(self):
         self.__window.destroy()
-
-
-
-# BluetoothSettingUI().start()
