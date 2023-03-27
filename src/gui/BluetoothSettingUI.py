9f55cdbedbca6c04a9010966925e1de8e3e4939d
p-chanmin <pc_m111@naver.com>
2023-03-27 23:49:56 +0900

    #19 블루투스 연결 구현

--- src/gui/BluetoothSettingUI.py
+++ src/gui/BluetoothSettingUI.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from tkinter import messagebox
 import asyncio
 import threading
 from bleak import BleakScanner
@@ -28,6 +29,69 @@ class SearchBluetooth(threading.Thread):
         self.bluetooth_ui.search_button.config(state=tk.NORMAL)
         loop.close()
 
+class ConnectBluetooth(threading.Thread):
+    __instance = None  # Singleton 인스턴스를 저장할 클래스 변수
+
+    def __new__(cls, bluetooth_ui):
+        if cls.__instance is None:
+            cls.__instance = super().__new__(cls)
+            cls.__instance.bluetooth_ui = bluetooth_ui
+        return cls.__instance
+
+    def __init__(self, bluetooth_ui):
+        super().__init__()
+        self.bluetooth_ui = bluetooth_ui
+
+    async def connect_device(self):
+        if(self.bluetooth_ui.client is not None and self.bluetooth_ui.client.is_connected):
+            try:
+                print(f"Disonnecting to {self.bluetooth_ui.client}")
+                await self.bluetooth_ui.client.disconnect()
+                print("Disonnecting Complete")
+                self.bluetooth_ui.connect_text.set("블루투스 연결 상태: Disconnected")
+                self.bluetooth_ui.printer_text.set(f"프린터 : 없음")
+                self.bluetooth_ui.connect_button_text.set("Connect")
+                self.bluetooth_ui.client = None
+            except Exception as e:
+                print(f"Failed to disconnect to {self.bluetooth_ui.client}: {e}")
+        else:
+            selected_device = self.bluetooth_ui.device_listbox.get(self.bluetooth_ui.device_listbox.curselection())
+            if selected_device in self.bluetooth_ui.devices:
+                print(f"Connecting to {selected_device} - {self.bluetooth_ui.devices[selected_device]}")
+                address = self.bluetooth_ui.devices[selected_device]
+                try:
+                    self.bluetooth_ui.client = BleakClient(address)
+                    await self.bluetooth_ui.client.connect()
+                    print(f"Connected to {selected_device}")
+                    # 연결 완료 시
+                    self.bluetooth_ui.homeclassInstance.printer_text.set(f"프린터 : {selected_device}")
+                    self.bluetooth_ui.connect_text.set("블루투스 연결 상태: Connected")
+                    self.bluetooth_ui.printer_text.set(f"프린터 : {selected_device}")
+                    self.bluetooth_ui.connect_button_text.set("Disconnect")
+
+                    services = await self.bluetooth_ui.client.get_services()
+                    # 서비스내에 있는 캐릭터리스틱 정보 보기
+                    for service in services:
+                        for characteristic in service.characteristics:
+                            if ('write' in characteristic.properties):
+                                self.bluetooth_ui.write_uuid = characteristic.uuid
+                            elif ('notify' in characteristic.properties):
+                                self.bluetooth_ui.notify_uuid = characteristic.uuid
+
+                    print(f"write_uuid : {self.bluetooth_ui.write_uuid}")
+                    print(f"notify_uuid : {self.bluetooth_ui.notify_uuid}")
+
+                except Exception as e:
+                        print(f"Failed to connect to {selected_device}: {e}")
+                        messagebox.showwarning("Error", f"{selected_device} : 디바이스 연결에 실패했습니다.\n재시도 해주세요.")
+                        self.bluetooth_ui.connect_text.set("블루투스 연결 상태: Disconnected")
+            else:
+                print(f"No device selected")
+    def run(self):
+        loop = asyncio.new_event_loop()  # 이벤트 루프를 얻음
+        loop.run_until_complete(self.connect_device())
+        self.bluetooth_ui.connect_button.config(state=tk.NORMAL)
+
 class BluetoothSettingUI:
     __instance = None  # Singleton 인스턴스를 저장할 클래스 변수
 
@@ -36,10 +100,11 @@ class BluetoothSettingUI:
             cls.__instance = super().__new__(cls)
             cls.__instance.devices = {}
             cls.__instance.client = None
+            cls.__instance.write_uuid = ''
+            cls.__instance.notify_uuid = ''
+            cls.__instance.homeclassInstance = homeclassInstance
         cls.__instance.__window = cls.__instance.__createUI(homeclassInstance)
         return cls.__instance
-
-
 
     def __createUI(self, homeclassInstance):
         window = tk.Tk()
@@ -49,25 +114,6 @@ class BluetoothSettingUI:
         # create a label for the Bluetooth device selection
         device_label = tk.Label(window, text="Select Bluetooth Device:")
         device_label.pack()
-
-        async def connect_device():
-            selected_device = self.device_listbox.get(self.device_listbox.curselection())
-            if selected_device in self.devices:
-                print(f"Connecting to {selected_device} - {self.devices[selected_device]}")
-                address = self.devices[selected_device]
-                try:
-                    self.client = BleakClient(address)
-                    await self.client.connect()
-                    print(f"Connected to {selected_device}")
-                    # 연결 완료 시
-                    homeclassInstance.printer_text.set(f"프린터 : {selected_device}")
-                    self.connect_text.set("블루투스 연결 상태: Connected")
-                    self.printer_text.set(f"프린터 : {selected_device}")
-                except Exception as e:
-                    print(f"Failed to connect to {selected_device}: {e}")
-                    self.connect_text.set("블루투스 연결 상태: Disconnected")
-            else:
-                print(f"No device selected")
 
         def on_select(event):
             selected_device = self.device_listbox.get(self.device_listbox.curselection())
@@ -82,25 +128,36 @@ class BluetoothSettingUI:
         button_frame = tk.Frame(window)
         button_frame.pack()
 
-        async def search_button_callback():
+        def search_button_callback():
             self.search_button.config(state=tk.DISABLED)
-            # await search_devices()
             thread = SearchBluetooth(self)
             thread.start()
 
+        def connect_button_callback():
+            self.connect_button.config(state=tk.DISABLED)
+            thread = ConnectBluetooth(self)
+            thread.start()
+
         # create a button to search to the Bluetooth device
-        self.search_button = tk.Button(button_frame, text="Search", command=lambda: asyncio.run(search_button_callback()))
+        self.search_button = tk.Button(button_frame, text="Search", command=search_button_callback)
         self.search_button.pack(side=tk.LEFT, padx=10)
 
+        self.connect_button_text = tk.StringVar(master=window)
+        if (self.client is not None and self.client.is_connected):
+            self.connect_button_text.set("Disconnect")
+        else:
+            self.connect_button_text.set("Connect")
         # create a button to connect to the selected Bluetooth device
-        connect_button = tk.Button(button_frame, text="Connect", command=lambda: asyncio.run(connect_device()))
-        connect_button.pack(side=tk.LEFT, padx=10)
+        self.connect_button = tk.Button(button_frame, textvariable=self.connect_button_text, command=connect_button_callback)
+        self.connect_button.pack(side=tk.LEFT, padx=10)
 
         self.connect_text = tk.StringVar(master=window)
-        if(self.client is None):
-            self.connect_text.set("블루투스 연결 상태: Disconnected")
-        else:
+        if(self.client is not None and self.client.is_connected):
             self.connect_text.set("블루투스 연결 상태: Connected")
+
+        else:
+            self.connect_text.set("블루투스 연결 상태: Disconnected")
+            self.homeclassInstance.printer_text.set(f"프린터 : 없음")
         # create a label for the Bluetooth status
         status_label = tk.Label(window, textvariable=self.connect_text)
         status_label.pack()
